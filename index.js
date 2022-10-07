fb13d1d0bab2187ebb283518e9c6b2f8583acc48
Luca <lukkino31@gmail.com>
2022-10-07 18:15:17 +0200

    adding interaction with js

--- index.js
+++ index.js
@@ -2,3 +2,29 @@
 const myButton = document.querySelector("button");
 
 myButton.onclick = function () {alert('URL')};
+
+const handleclickHunan = () => {
+    document.querySelector('#rec-show').src='hunan_sauce.html'
+}
+
+const handleclickGinger = () => {
+    document.querySelector('#rec-show').src='./recipe/ginger_garlic.html'
+}
+
+const handleclickShawarma = () => {
+    document.querySelector('#rec-show').src='./recipe/Shawarma_spice.html'
+}
+
+
+const handleclickZaatar = () => {
+    document.querySelector('#rec-show').src='./recipe/Zaatar.html'
+}
+
+const handleclickChickpeas = () => {
+    document.querySelector('#rec-show').src='./recipe/Pressure_chickpeas.html'
+}
+document.querySelector('#hunan-button').addEventListener('click', handleclickHunan)
+document.querySelector('#ginger-button').addEventListener('click', handleclickGinger)
+document.querySelector('#chickpeas-button').addEventListener('click', handleclickShawarma)
+document.querySelector('#shawarma-button').addEventListener('click', handleclickZaatar)
+document.querySelector('#zaatar-button').addEventListener('click', handleclickChickpeas)
